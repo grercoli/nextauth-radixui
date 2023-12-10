39f3fcd6e4034663ff7c7528716fcb5119b4c375
Gustavo Ercoli <ruben.ercoli@gmail.com>
2023-12-10 20:41:39 -0300

    feat: first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@radix-ui/themes';
+import { BookmarkIcon } from '@radix-ui/react-icons'
+
+const LoginPage = () => {
+  return (
+    <div>
+      LoginPage
+      <Button>
+        <BookmarkIcon />Login
+      </Button>
+    </div>
+  )
+}
+
+export default LoginPage
